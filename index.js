1e2970ddc57b4fb85eaccab41c13d9663e52ca7e
Nick <nmcarducci@gmail.com>
2023-06-04 10:51:24 -0400

    ok

--- index.js
+++ index.js
@@ -224,7 +224,7 @@ attach
     const companyID = req.body.companyIDToken.split(":")[0];
 
     const selectAccount =
-      "select * from Account where Classification = 'Expense'"; // where Metadata.CreateTime > '2014-12-31'";
+      "select * from Account where Classification LIKE 'Expense'"; // where Metadata.CreateTime > '2014-12-31'";
     const accounts = await oauthClient.makeApiCall({
       url:
         url +
@@ -247,7 +247,7 @@ attach
         error: "no go accounts by oauth"
       });
     const selectVendor =
-      "select * from vendor where Metadata.CreateTime > '2014-12-31'";
+      "select * from vendor"// where Metadata.CreateTime > '2014-12-31'";
     const vendors = await oauthClient.makeApiCall({
       url:
         url +
@@ -270,7 +270,7 @@ attach
         error: "no go vendors by oauth"
       });
     const selectCustomer =
-      "select * from Customer where Metadata.CreateTime > '2014-12-31'";
+      "select * from Customer"// where Metadata.CreateTime > '2014-12-31'";
     const customers = await oauthClient.makeApiCall({
       url:
         url +
